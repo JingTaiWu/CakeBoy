dc783b21c0c1d87eef90f35199a065d12be1b240
Marist User <maristuser@lt158m-05.local>
2014-10-07 21:38:16 -0400

    Initial Commit

---
+++ Cakeboy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cakeboy
+//
+//  Created by Marist User on 10/7/14.
+//  Copyright JingtaiWu 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
